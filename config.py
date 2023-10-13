0926ba0b4646bca0018af0b5a71ebef18dcf5fb6
Ram S <ram.senth@berkeley.edu>
2023-10-13 15:00:18 -0700

    Original EDA notebook from Isabel

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    def __init__(self):
+        self.ESSAY_COL = 'essay'
+        self.CORRECTED_COL = 'corrected'
+        self.INPUT_COL = 'input_text'
+        self.OUTPUT_COL = 'target_text'
+        self.RAW_DATA_FOLDER = './raw_data/csv'
---
+++ raw_data/README.md
@@ -0,0 +1,2 @@
+# Raw Data
+CSV.zip is compressed version of [CSV files](https://github.com/ucdaviscl/cowsl2h/tree/master/csv) from COWS2H dataset. Before running code explode the csv.zip.
