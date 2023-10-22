67d4b7798f4df3ffb7ddd47cd96acdd41d6843d2
Ram S <ram.senth@berkeley.edu>
2023-10-22 14:03:17 -0700

    EDA: data verification and parsing tree generation

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ class Config:
         self.INPUT_COL = 'input_text'
         self.OUTPUT_COL = 'target_text'
         self.RAW_DATA_FOLDER = './raw_data/csv'
+        self.PROCESSED_DATA_FOLDER = './processed_data'
+        self.STANFORD_PARSERS_FOLDER = './stanford-parser-full-2020-11-17'
+        self.STANFORD_CORENLP_FOLDER = './stanford-corenlp-4.5.5'
+        self.EXPLAINABLE_GEC_DATA_FOLDER = './Explainable_GEC/data/json'
