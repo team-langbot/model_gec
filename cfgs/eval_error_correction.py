9d7714fb243f0f26a483effbfac47e91cdad0530
Ram S <ram.senth@berkeley.edu>
2023-11-03 15:22:50 -0700

    Added smaller dataset with evidence words annotated

--- cfgs/eval_error_correction.py
+++ cfgs/eval_error_correction.py
@@ -2,9 +2,9 @@ config = {
     "exp_name" : '/error_correction/ner_1e5_bs32_bert_large/checkpoint-n', # best checkpoint path
     "eval_res_dir" : 'eval/dev.pkl',
     "only_inference" : ['dev'], # choose from ['dev', 'test'] or both
-    "train_dev_data": ['processed_data/bert_train.pkl',
-                       'processed_data/bert_dev.pkl',
-                       'processed_data/bert_test.pkl',],
+    "train_dev_data": ['processed_data/bert_train_small.pkl',
+                       'processed_data/bert_dev_small.pkl',
+                       'processed_data/bert_test_small.pkl',],
     "max_correction_embeddings": 0, # set 0 if no correction embeddings, else 3
     "debug": False,
     "max_seq_length": 256,
--- cfgs/train_error_correction_ce.py
+++ cfgs/train_error_correction_ce.py
@@ -2,9 +2,9 @@ config = {
     "exp_name" : '/error_correction_ce/ner_1e5_bs32_bert_large',
     "eval_res_dir": '',
     "only_inference" : None,
-    "train_dev_data": ['processed_data/bert_train.pkl',
-                       'processed_data/bert_dev.pkl',
-                       'processed_data/bert_test.pkl',],
+    "train_dev_data": ['processed_data/bert_train_small.pkl',
+                       'processed_data/bert_dev_small.pkl',
+                       'processed_data/bert_test_small.pkl',],
     "max_correction_embeddings": 3, # set 0 if no correction embeddings, else 3
     "lr": 1e-5,
     "debug": False,
